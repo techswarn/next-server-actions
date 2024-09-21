c5071090d40b0808a4c92e8facb1047187132115
techswarn <tech.swarns@gmail.com>
2024-09-21 11:01:57 +0530

    code update

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,6 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
-
+export const dynamic = "force-dynamic";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
